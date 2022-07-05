e76da1eac1518f4305b728b8b0b919e61f20b291
Andres Rivero <andres19rivero@gmail.com>
2022-07-05 19:57:58 -0400

    control & validate the search record

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GifsService {
-    private _record: string[] = [];
+  private _record: string[] = [];
 
-    get record() {
-        return [...this._record]
+  get record() {
+    return [...this._record];
+  }
+
+  searchGifs(query: string) {
+    query = query.trim().toLowerCase();
+    if (!this._record.includes(query)) {
+      this._record.unshift(query);
+      this._record = this._record.splice(0, 10);
     }
+    console.log(this._record);
+  }
 
-    searchGifs(query: string) {
-        this._record.unshift(query)
-        console.log(this._record)
-    }
-
-  constructor() { }
+  constructor() {}
 }
