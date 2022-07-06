3560ffc92e414fcc0aec715b6bd151d4a061064e
Andres Rivero <andres19rivero@gmail.com>
2022-07-05 20:25:34 -0400

    http service added

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private apiKey: string = 'hHyuKMSGLu4JDBax2tSVoGqkOrxD3DmZ';
   private _record: string[] = [];
 
   get record() {
     return [...this._record];
   }
+  constructor(private http: HttpClient) {}
 
   searchGifs(query: string) {
     query = query.trim().toLowerCase();
@@ -16,8 +19,8 @@ export class GifsService {
       this._record.unshift(query);
       this._record = this._record.splice(0, 10);
     }
-    console.log(this._record);
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=hHyuKMSGLu4JDBax2tSVoGqkOrxD3DmZ&q=dragon ball z&limit=10').subscribe(( resp: any ) => {
+      console.log(resp.data)
+    })
   }
-
-  constructor() {}
 }
