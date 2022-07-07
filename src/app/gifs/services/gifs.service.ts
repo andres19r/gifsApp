1cca15ed2b22823fd7090b68ec4c80544267f6ac
Andres Rivero <andres19rivero@gmail.com>
2022-07-07 11:59:54 -0400

    use http params for the request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
@@ -7,6 +7,7 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 })
 export class GifsService {
   private apiKey: string = 'hHyuKMSGLu4JDBax2tSVoGqkOrxD3DmZ';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
   private _record: string[] = [];
 
   public results: Gif[] = [];
@@ -26,10 +27,14 @@ export class GifsService {
       this._record = this._record.splice(0, 10);
       localStorage.setItem('record', JSON.stringify(this._record));
     }
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+
     this.http
-      .get<SearchGifsResponse>(
-        `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`
-      )
+      .get<SearchGifsResponse>(`${this.serviceUrl}/search`, { params })
       .subscribe((resp) => {
         console.log(resp.data);
         this.results = resp.data;
