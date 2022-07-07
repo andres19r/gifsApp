0c711608313bf8bff717d484c084868aa6522cef
Andres Rivero <andres19rivero@gmail.com>
2022-07-06 21:42:06 -0400

    save querys in local storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,13 +14,19 @@ export class GifsService {
   get record() {
     return [...this._record];
   }
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._record = JSON.parse(localStorage.getItem('record')!) || []
+    // if(localStorage.getItem('record')) {
+    //   this._record = JSON.parse(localStorage.getItem('record')!)
+    // }
+  }
 
   searchGifs(query: string) {
     query = query.trim().toLowerCase();
     if (!this._record.includes(query)) {
       this._record.unshift(query);
       this._record = this._record.splice(0, 10);
+      localStorage.setItem('record', JSON.stringify(this._record));
     }
     this.http
       .get<SearchGifsResponse>(
